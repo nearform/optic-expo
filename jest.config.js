c405773da6ffa5d6a9d89c98a1db073d8f548651
Salman <salmanmitha@gmail.com>
2021-10-19 15:14:21 +0530

    chore: add typescript

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo/universal',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   projects: [
     { preset: 'jest-expo/ios', setupFiles: ['./jest.setup.js'] },
     { preset: 'jest-expo/android', setupFiles: ['./jest.setup.js'] },
