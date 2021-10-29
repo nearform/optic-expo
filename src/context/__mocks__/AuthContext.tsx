9c1e9eea0ae0c3b077bb9640cecc57ee0f071dda
Salman <salmanmitha@gmail.com>
2021-10-29 19:09:04 +0530

    fix: static user object in tests

--- src/context/__mocks__/AuthContext.tsx
+++ src/context/__mocks__/AuthContext.tsx
@@ -1,11 +1,13 @@
+const mockedUser = {
+  name: 'John',
+  uid: '11-22-33-44',
+  idToken: '1213',
+}
+
 export const useAuth = jest.fn(() => {
   return {
     loading: false,
-    user: {
-      name: 'John',
-      uid: '11-22-33-44',
-      idToken: '1213',
-    },
+    user: mockedUser,
     handleLogin: jest.fn(),
     handleLogout: jest.fn(),
   }
