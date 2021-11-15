79633b8654f23996bbae1bd4fbcb76a0f17a1ec9
Kevin Nolan <kevin.nolan@nearform.com>
2021-11-15 18:03:58 +0000

    chore: allow firebase to be configurable

--- app.config.js
+++ app.config.js
@@ -12,6 +12,14 @@ export default {
     icon: './src/assets/icon.png',
     extra: {
       apiUrl: process.env.API_URL,
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGEING_SENDER_ID,
+      appId: process.env.APP_ID,
+      clientId: process.env.CLIENT_ID,
     },
     splash: {
       image: './src/assets/splash.png',
