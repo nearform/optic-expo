776833f3aaf750e8b4509ca456e691faaff37a88
Kevin Nolan <kevin.nolan@nearform.com>
2021-11-16 09:01:30 +0000

    chore: allow firebase to be configured (#217)

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -8,17 +8,40 @@ import React, {
 import * as Google from 'expo-auth-session/providers/google'
 import firebase from 'firebase'
 import * as WebBrowser from 'expo-web-browser'
+import Constants from 'expo-constants'
 
 import { User } from '../types'
 
+interface FirebaseSettings {
+  apiKey: string
+  authDomain: string
+  databaseURL: string
+  projectId: string
+  storageBucket
+  messagingSenderId: string
+  appId: string
+  clientId: string
+}
+
+const {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  clientId,
+} = Constants.manifest?.extra as FirebaseSettings
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBFiuUulmuQ7Pv1VvxpUQB01AWCEQhIToA',
-  authDomain: 'npm-otp-f6bfc.firebaseapp.com',
-  databaseURL: 'https://npm-otp-f6bfc.firebaseio.com',
-  projectId: 'npm-otp-f6bfc',
-  storageBucket: 'npm-otp-f6bfc.appspot.com',
-  messagingSenderId: '230076165693',
-  appId: '1:230076165693:web:a04f9ad6f64be8ec248454',
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
 }
 
 if (!firebase.apps.length) {
@@ -47,8 +70,7 @@ export const AuthProvider: React.FC<AuthenticationProviderProps> = ({
   const [user, setUser] = useState<User>()
 
   const [, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId:
-      '230076165693-0mj3vb13158tnru89f1re89m9o94g8e7.apps.googleusercontent.com',
+    clientId,
   })
 
   useEffect(() => {
