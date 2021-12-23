7093f50f364d7cf035467e7c367ff7b4976d26c9
Simone Busoli <simone.busoli@nearform.com>
2021-12-23 14:45:37 +0100

    chore: upgrade expo sdk to 44 (#301)

--- app.config.js
+++ app.config.js
@@ -17,7 +17,7 @@ export default {
       databaseURL: process.env.DATABASE_URL,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGEING_SENDER_ID,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
       clientId: process.env.CLIENT_ID,
     },
