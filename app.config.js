fb78cb36f78899f21a9b8344d345c6ffca5ec133
Luis Confraria <lconfraria@gmail.com>
2022-09-05 14:48:03 +0100

    chore: add add support for EAS build and submit

--- app.config.js
+++ app.config.js
@@ -1,44 +1,47 @@
 import 'dotenv/config'
 
 export default {
-  expo: {
-    name: 'optic-expo',
-    slug: 'optic-expo',
-    owner: 'nearform',
-    scheme: 'optic-expo',
-    entryPoint: './src/index.js',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './src/assets/icon.png',
-    extra: {
-      apiUrl: process.env.API_URL,
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      clientId: process.env.CLIENT_ID,
+  name: 'optic-expo',
+  slug: 'optic-expo',
+  owner: 'nearform',
+  scheme: 'optic-expo',
+  entryPoint: './src/index.js',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './src/assets/icon.png',
+  extra: {
+    apiUrl: process.env.API_URL,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    clientId: process.env.CLIENT_ID,
+    eas: {
+      projectId: process.env.EAS_PROJECT_ID,
     },
-    splash: {
-      image: './src/assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff',
-    },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
-    assetBundlePatterns: ['**/*'],
-    platforms: ['android', 'ios'],
-    ios: {
-      supportsTablet: true,
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './src/assets/adaptive-icon.png',
-        backgroundColor: '#FFFFFF',
-      },
+  },
+  splash: {
+    image: './src/assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  platforms: ['android', 'ios'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.nearfrom.optic',
+  },
+  android: {
+    package: 'com.nearfrom.optic',
+    adaptiveIcon: {
+      foregroundImage: './src/assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
     },
   },
 }
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import { registerRootComponent } from 'expo'
 
 global.Buffer = global.Buffer || require('buffer').Buffer
 
-import App from './App'
+import App from './src/App'
 
 registerRootComponent(App)
