acc20b2fe581e1e926b2651c4aef4ce50fcd8b8f
Luis Confraria <lconfraria@gmail.com>
2022-09-16 18:09:42 +0100

    align loggin with existing google login button

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react'
 import * as WebBrowser from 'expo-web-browser'
-import { Platform, View, StyleSheet, Image } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, Platform, StyleSheet, View } from 'react-native'
 import { Button } from 'react-native-paper'
 import * as AppleAuthentication from 'expo-apple-authentication'
 
-import theme from '../lib/theme'
-import { useAuth } from '../context/AuthContext'
-import { Typography } from '../components/Typography'
 import Logo from '../components/Logo'
+import { Typography } from '../components/Typography'
+import { useAuth } from '../context/AuthContext'
+import theme from '../lib/theme'
 
 const styles = StyleSheet.create({
   container: {
@@ -24,15 +24,19 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     alignItems: 'center',
     padding: theme.spacing(1),
+    marginTop: theme.spacing(1),
   },
 })
 
 type AuthScreenProps = unknown
 
 export const AuthScreen: React.FC<AuthScreenProps> = () => {
+  const [appleLoginEnabled, setAppleLoginEnabled] = useState(false)
   const { handleLoginGoogle, handleLoginApple } = useAuth()
 
   useEffect(() => {
+    AppleAuthentication.isAvailableAsync().then(setAppleLoginEnabled)
+
     if (Platform.OS === 'web') {
       return
     }
@@ -63,28 +67,40 @@ export const AuthScreen: React.FC<AuthScreenProps> = () => {
           Grant your favorite automated tools an OTP when they need it!
         </Typography>
       </View>
-      <Button
-        style={styles.button}
-        accessibilityLabel="login"
-        mode="contained"
-        icon={({ size }) => (
-          <Image
-            source={require('../assets/google.png')}
-            style={{ width: size, height: size }}
-          />
+      <View>
+        <Button
+          style={styles.button}
+          accessibilityLabel="login"
+          mode="contained"
+          icon={({ size }) => (
+            <Image
+              source={require('../assets/google.png')}
+              style={{ width: size, height: size }}
+            />
+          )}
+          onPress={handleLoginGoogle}
+          color={theme.colors.surface}
+        >
+          Sign in with Google
+        </Button>
+        {appleLoginEnabled && (
+          <Button
+            style={styles.button}
+            accessibilityLabel="login"
+            mode="contained"
+            icon={({ size }) => (
+              <Image
+                source={require('../assets/apple.png')}
+                style={{ width: size, height: size }}
+              />
+            )}
+            onPress={handleLoginApple}
+            color={theme.colors.surface}
+          >
+            Sign in with Apple
+          </Button>
         )}
-        onPress={handleLoginGoogle}
-        color={theme.colors.surface}
-      >
-        Sign in with Google
-      </Button>
-      <AppleAuthentication.AppleAuthenticationButton
-        buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-        buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.WHITE}
-        cornerRadius={5}
-        style={{ width: 200, height: 44 }}
-        onPress={handleLoginApple}
-      />
+      </View>
       <Logo />
     </View>
   )
