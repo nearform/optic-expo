1b0bafc33b8710ecb2145a3037ed561c57b587cb
Luis Confraria <lconfraria@gmail.com>
2022-09-20 12:44:25 +0100

    fix: add more detail to camera usage

--- app.config.js
+++ app.config.js
@@ -60,6 +60,10 @@ export default {
     bundleIdentifier: config.APP_PACKAGE_ID,
     buildNumber: String(pkg.versionCode),
     usesAppleSignIn: true,
+    infoPlist: {
+      NSCameraUsageDescription:
+        'This app uses the camera to scan QR codes with OTP secrets',
+    },
   },
   android: {
     versionCode: pkg.versionCode,
