76454b01f6be17d7a1a2d6d09fd4ead4264bc0cd
Andrea Pacino <andrea.pacino87@gmail.com>
2022-10-07 13:29:15 +0200

    fix: clean secrets before securely storing them

--- src/screens/TypeScreen.tsx
+++ src/screens/TypeScreen.tsx
@@ -43,7 +43,8 @@ export const TypeScreen: React.FC<TypeScreenProps> = ({ navigation }) => {
   const [account, setAccount] = useState(user.name || '')
 
   const handleAddSecretButtonPress = async () => {
-    await add({ uid: user.uid, secret, account, issuer })
+    const cleanedSecret = secret.trim().replace(/ /g, '').toUpperCase()
+    await add({ uid: user.uid, secret: cleanedSecret, account, issuer })
     navigation.navigate('Home')
   }
 
