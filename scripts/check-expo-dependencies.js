46f4d4de22cd9cd5d1e1703fb55b724b305b9aa4
Ana Lemos <ana.lemos@nearform.com>
2023-04-20 14:56:16 -0300

    fix: use dependabot-metadata to get dependency names being updated

--- scripts/check-expo-dependencies.js
+++ scripts/check-expo-dependencies.js
@@ -1,7 +1,9 @@
-module.exports = async ({ context, github, fetch, prTitle }) => {
+module.exports = async ({ context, github, fetch, dependencyNames }) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const expoVersion = require('../package.json').dependencies.expo
-  const expoVersionSanitized = expoVersion.replaceAll('~', '')
+  const expoVersionSanitized = expoVersion
+    .replaceAll('~', '')
+    .replaceAll('^', '')
 
   const majorExpoVersion = `${expoVersionSanitized.substring(
     0,
@@ -12,13 +14,17 @@ module.exports = async ({ context, github, fetch, prTitle }) => {
   const expoDataOutput = await expoData.json()
 
   const { relatedPackages } = expoDataOutput.data.sdkVersions[majorExpoVersion]
+  const dependencyNamesList = dependencyNames.split(',')
 
   const shouldClosePR = [
     ...Object.keys(relatedPackages),
     'react',
     'react-native',
     'expo',
-  ].some(dependency => prTitle.includes(dependency))
+  ].some(
+    // checks if the list of dependencies being updated contains a related package
+    package => dependencyNamesList.includes(package)
+  )
 
   if (shouldClosePR) {
     // close PR
